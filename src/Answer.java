efab8a0079925f3c99bd28d9cca8e5ec32384191
Aleksandr Jefimov <aljefi@ttu.ee>
2021-09-09 20:55:38 +0300

    kt0
    Signed-off-by: ajefi <ajefimo2001@gmail.com>

--- src/Answer.java
+++ src/Answer.java
@@ -1,33 +1,63 @@
 import java.util.*;
 
 public class Answer {
-
    public static void main (String[] param) {
-
       // TODO!!! Solutions to small problems 
       //   that do not need an independent method!
-    
+
       // conversion double -> String
+      double param1 = 1.11;
+      String convertedToString = String.valueOf(param1);
+      System.out.println(convertedToString);
 
       // conversion String -> int
+      String param2 = "1234";
+      try {
+         param2 = String.valueOf(Integer.parseInt(param2));
+      } catch (NumberFormatException e) {
+         e.printStackTrace(System.out);
+      }
+      System.out.println(param2);
 
       // "hh:mm:ss"
+      String[] a = new Date(System.currentTimeMillis()).toString().split(" ");
+      System.out.println("Time: " + a[3]);
 
       // cos 45 deg
+      double inDegrees = 45.0;
+      double inRadians = Math.toRadians(inDegrees);
+      System.out.println("cos45: " + Math.cos(inRadians));
 
       // table of square roots
+      for (double i = 0; i <= 100;) {
+         System.out.println(i + "\t" + Math.sqrt(i));
+         i += 5;
+      }
+
 
       String firstString = "ABcd12";
       String result = reverseCase (firstString);
       System.out.println ("\"" + firstString + "\" -> \"" + result + "\"");
 
       // reverse string
+      String revS = "1234ab";
+      StringBuilder sb = new StringBuilder(revS);
+      sb.reverse();
+      System.out.println(sb);
 
       String s = "How  many	 words   here";
       int nw = countWords (s);
       System.out.println (s + "\t" + String.valueOf (nw));
 
       // pause. COMMENT IT OUT BEFORE JUNIT-TESTING!
+      long t0 = System.nanoTime();
+      try {
+         Thread.sleep(3000);
+      } catch (InterruptedException e) {
+         e.printStackTrace();
+      }
+      long t1 = System.nanoTime();
+      System.out.println("Pause: " + (t1-t0) + " ns.");
 
       final int LSIZE = 100;
       ArrayList<Integer> randList = new ArrayList<Integer> (LSIZE);
@@ -37,12 +67,33 @@ public class Answer {
       }
 
       // minimal element
-
+      Integer minNum = 1000;
+      for (Integer i : randList) {
+         // if i < maxNum ===> -1
+         if (i.compareTo(minNum) < 0)
+            minNum = i;
+      }
+      System.out.println("Minimum: " + minNum);
       // HashMap tasks:
       //    create
+      HashMap<Integer, String> hmap = new HashMap<>();
+      hmap.put(1, "Maths");
+      hmap.put(2, "Chemistry");
+      hmap.put(3, "Foreign language");
+      hmap.put(4, "Eesti keel");
+      hmap.put(5, "Algebra");
       //    print all keys
+      for (Integer i :
+              hmap.keySet()) {
+         System.out.println(i);
+      }
       //    remove a key
+      hmap.remove(3);
       //    print all pairs
+      for (Integer i : 
+              hmap.keySet()) {
+         System.out.println(i + ": " + hmap.get(i));
+      }
 
       System.out.println ("Before reverse:  " + randList);
       reverseList (randList);
@@ -57,9 +108,15 @@ public class Answer {
     * @throws NoSuchElementException if <code> a </code> is empty.
     */
    static public <T extends Object & Comparable<? super T>>
-         T maximum (Collection<? extends T> a) 
+         T maximum (Collection<? extends T> a)
             throws NoSuchElementException {
-      return null; // TODO!!! Your code here
+      T maxNum = (T) a.toArray()[0];
+      for (T j : a) {
+         // if j > maxNum ==> true
+         if (j.compareTo(maxNum) > 0)
+            maxNum = j;
+      }
+      return maxNum;
    }
 
    /** Counting the number of words. Any number of any kind of
@@ -68,7 +125,8 @@ public class Answer {
     * @return number of words in the text
     */
    public static int countWords (String text) {
-      return 0; // TODO!!! Your code here
+      StringTokenizer tokens = new StringTokenizer(text);
+      return tokens.countTokens();
    }
 
    /** Case-reverse. Upper -> lower AND lower -> upper.
@@ -76,14 +134,24 @@ public class Answer {
     * @return processed string
     */
    public static String reverseCase (String s) {
-      return null; // TODO!!! Your code here
-   }
+      //converting to charArray
+      char[] sToChars = s.toCharArray();
+         for (int i = 0; i < sToChars.length; i++) {
+            // ternary operator
+            sToChars[i] = Character.isLowerCase(sToChars[i])
+                    ? Character.toUpperCase(sToChars[i])
+                    : Character.toLowerCase(sToChars[i]);
+         }
+         return new String(sToChars);
+      }
 
    /** List reverse. Do not create a new list.
     * @param list list to reverse
     */
    public static <T extends Object> void reverseList (List<T> list)
       throws UnsupportedOperationException {
-         // TODO!!! Your code here
+      for (int i = 0; i < list.size(); i++) {
+         list.add(i, list.remove(list.size() - 1));
+      }
    }
 }
